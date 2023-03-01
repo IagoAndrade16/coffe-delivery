2761135977395553371da0a57cc6dc4b740464ff
Iago Alexandre <iagoaap@outlook.com>
2023-03-01 06:56:25 -0300

    list of coffees

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,12 @@
 import { Coffee, Package, ShoppingCartSimple, Timer } from 'phosphor-react'
 import {
+  CoffeCard,
+  CoffeCardBuy,
+  CoffeCardBuyActions,
+  CoffeCardBuyActionsCounter,
+  CoffeCardBuyText,
+  CoffeeList,
+  CoffeeListTitle,
   Intro,
   IntroImage,
   IntroTitle,
@@ -13,49 +20,64 @@ import {
 
 export function Home() {
   return (
-    <Intro>
-      <IntroTitle>
-        <IntroTitleMain>
-          Encontre o café perfeito para qualquer hora do seu dia.
-        </IntroTitleMain>
-        <IntroTitleSubtitle>
-          Com o Coffee Delivery você pode receber seu café onde estiver, a
-          qualquer hora
-        </IntroTitleSubtitle>
-      </IntroTitle>
+    <div>
+      <Intro>
+        <IntroTitle>
+          <IntroTitleMain>
+            Encontre o café perfeito para qualquer hora do seu dia.
+          </IntroTitleMain>
+          <IntroTitleSubtitle>
+            Com o Coffee Delivery você pode receber seu café onde estiver, a
+            qualquer hora
+          </IntroTitleSubtitle>
+        </IntroTitle>
 
-      <IntroImage>
-        <img src="../../../assets/coffe-delivery-back.svg" alt="" />
-      </IntroImage>
+        <IntroImage>
+          <img src="../../../assets/coffe-delivery-back.svg" alt="" />
+        </IntroImage>
 
-      <Items>
-        <Item>
-          <ItemIcon>
-            <ShoppingCartSimple size={24} weight="fill" color="#FFF" />
-          </ItemIcon>
-          <ItemText>Compra simples e segura</ItemText>
-        </Item>
-        <Item>
-          <ItemIcon>
-            <Timer size={24} weight="fill" color="#FFF" />
-          </ItemIcon>
-          <ItemText>Entrega rápida e rastreada</ItemText>
-        </Item>
+        <Items>
+          <Item>
+            <ItemIcon>
+              <ShoppingCartSimple size={24} weight="fill" color="#FFF" />
+            </ItemIcon>
+            <ItemText>Compra simples e segura</ItemText>
+          </Item>
+          <Item>
+            <ItemIcon>
+              <Timer size={24} weight="fill" color="#FFF" />
+            </ItemIcon>
+            <ItemText>Entrega rápida e rastreada</ItemText>
+          </Item>
 
-        <Item>
-          <ItemIcon>
-            <Package size={24} weight="fill" color="#FFF" />
-          </ItemIcon>
-          <ItemText>Embalagem mantém o café intacto</ItemText>
-        </Item>
+          <Item>
+            <ItemIcon>
+              <Package size={24} weight="fill" color="#FFF" />
+            </ItemIcon>
+            <ItemText>Embalagem mantém o café intacto</ItemText>
+          </Item>
 
-        <Item>
-          <ItemIcon background-color="#DBAC2C">
-            <Coffee size={24} weight="fill" color="#FFF" />
-          </ItemIcon>
-          <ItemText>O café chega fresquinho até você</ItemText>
-        </Item>
-      </Items>
-    </Intro>
+          <Item>
+            <ItemIcon background-color="#DBAC2C">
+              <Coffee size={24} weight="fill" color="#FFF" />
+            </ItemIcon>
+            <ItemText>O café chega fresquinho até você</ItemText>
+          </Item>
+        </Items>
+      </Intro>
+
+      <CoffeeList>
+        <CoffeeListTitle>Nossos cafés</CoffeeListTitle>
+
+        <CoffeCard>
+          <CoffeCardBuy>
+            <CoffeCardBuyText>R$9,90</CoffeCardBuyText>
+            <CoffeCardBuyActions>
+              <CoffeCardBuyActionsCounter></CoffeCardBuyActionsCounter>
+            </CoffeCardBuyActions>
+          </CoffeCardBuy>
+        </CoffeCard>
+      </CoffeeList>
+    </div>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -170,3 +170,137 @@ export const ItemText = styled.div`
   order: 1;
   flex-grow: 1;
 `
+
+export const CoffeeList = styled.div`
+  display: flex;
+  position: absolute;
+  height: 1645px;
+  left: 0px;
+  right: 0px;
+  top: 648px;
+`
+
+export const CoffeeListTitle = styled.div`
+  position: absolute;
+  width: 250px;
+  height: 42px;
+  left: calc(50% - 193px / 2 - 463.5px);
+  top: 32px;
+
+  /* Title/Title L */
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 800;
+  font-size: 32px;
+  line-height: 130%;
+  /* identical to box height, or 42px */
+
+  display: flex;
+  align-items: center;
+
+  /* Base/Subtitle */
+
+  color: #403937;
+`
+
+export const CoffeCard = styled.div`
+  display: flex;
+  position: absolute;
+  width: 256px;
+  height: 310px;
+  left: 864px;
+  top: 700px;
+
+  /* Base/Card */
+
+  background: #f3f2f2;
+  border-radius: 6px 36px;
+`
+
+export const CoffeCardBuy = styled.div`
+  /* Buy */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px;
+  gap: 29px;
+
+  position: absolute;
+  height: 38px;
+  left: 24px;
+  right: 24px;
+  bottom: 20px;
+`
+
+export const CoffeCardBuyText = styled.div`
+  width: 67px;
+  height: 31px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 800;
+  font-size: 20px;
+  line-height: 130%;
+  /* or 18px */
+
+  text-align: right;
+
+  /* Base/Text */
+
+  color: #574f4d;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`
+
+export const CoffeCardBuyActions = styled.div`
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: 8px;
+
+  width: 118px;
+  height: 38px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`
+
+export const CoffeCardBuyActionsCounter = styled.div`
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+  gap: 4px;
+
+  width: 72px;
+  height: 24px;
+
+  /* Base/Button */
+
+  background: #e6e5e5;
+  border-radius: 6px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`
